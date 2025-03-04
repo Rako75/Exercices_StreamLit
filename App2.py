d6206c878fb6d7a8f8c012bf7cfaaa61526d354d
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-03-04 13:49:08 +0100

    Create App2.py

---
+++ App2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Bienvenue sur mon application Streamlit !")
+
+# Champ de texte pour entrer le nom
+nom = st.text_input("Entrez votre nom:")
+
+# Affichage du message personnalisé
+if nom:
+    st.write(f"Bienvenue, {nom} ! 😊")
+
+# Affichage d'une image
+st.image("image.jpg", caption="Bienvenue sur mon application !", use_column_width=True)
